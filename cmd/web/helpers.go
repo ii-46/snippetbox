1cb7ce1d5f30a074865a5a2f761ea6f56dff287b
Inthava <coci8750@gmail.com>
2023-10-16 09:29:47 +0700

    add error helper function

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt",
-	"net/http",
+	"fmt"
+	"net/http"
 	"runtime/debug"
 )
 
-func (app *application) serverError(w http.ResponseWriter, err Error) {
+func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Println(trace)
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServer)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
-	http.Error(w, http.StatusText(http.status), status)
+	http.Error(w, http.StatusText(status), status)
 }
