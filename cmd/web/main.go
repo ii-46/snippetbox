c3274670149149bacd9218d62e0005ba071ae492
Inthava <coci8750@gmail.com>
2023-10-27 08:51:52 +0700

    add db driver and  data source

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -14,10 +16,16 @@ type application struct {
 
 func main() {
 	adrr := flag.String("addr", ":4000", "network address")
+	dsn := flag.String("dsn", "web1:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
@@ -25,6 +33,16 @@ func main() {
 	mux := app.routes()
 	infoLog.Printf("starting server at %v\n", *adrr)
 	srv := &http.Server{Handler: mux, ErrorLog: errorLog, Addr: *adrr}
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
